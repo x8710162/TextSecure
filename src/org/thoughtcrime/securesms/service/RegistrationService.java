96c28ae9577985dfd7579e73a7a36872c592d550
Moxie Marlinspike <moxie@thoughtcrime.org>
2014-04-24 15:39:55 -0700

    Collapse SessionRecord, SessionState, and PreKeyRecord interfaces.

--- src/org/thoughtcrime/securesms/service/RegistrationService.java
+++ src/org/thoughtcrime/securesms/service/RegistrationService.java
@@ -21,6 +21,7 @@ import org.thoughtcrime.securesms.util.DirectoryHelper;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.whispersystems.libaxolotl.IdentityKey;
 import org.whispersystems.libaxolotl.state.PreKeyRecord;
+import org.whispersystems.libaxolotl.util.KeyHelper;
 import org.whispersystems.textsecure.crypto.MasterSecret;
 import org.whispersystems.textsecure.crypto.PreKeyUtil;
 import org.whispersystems.textsecure.push.ExpectationFailedException;
@@ -206,7 +207,7 @@ public class RegistrationService extends Service {
     int          registrationId = TextSecurePreferences.getLocalRegistrationId(this);
 
     if (registrationId == 0) {
-      registrationId = Util.generateRegistrationId();
+      registrationId = KeyHelper.generateRegistrationId();
       TextSecurePreferences.setLocalRegistrationId(this, registrationId);
     }
 
